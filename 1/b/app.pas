e9323bb5c4fa884bf837186b364a4799a3dc3935
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-03-14 09:55:37 -0300

    1)b) solved

--- 1/b/app.pas
+++ 1/b/app.pas
@@ -1,1 +1,14 @@
-ECHO is on.
+program app;
+
+var
+    inscriptos: Integer;
+    aprobados: Integer;
+begin
+
+    inscriptos := 160;
+    aprobados := 48;
+
+    Write(((aprobados/inscriptos)*100):2:2);
+    WriteLn('% de aprobados')
+
+end.
