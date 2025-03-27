5a0365f42ac3b1a6a8cb27e8d558f7c422063ba2
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-03-26 21:36:07 -0300

    small correction:

--- Practica 2/1/c/app.pas
+++ Practica 2/1/c/app.pas
@@ -3,17 +3,19 @@ program app;
 var
 
     number: Integer;
-
+    i : integer;
 begin
     
-    write('Introduce un numero: ');
-    readln(number);
-    repeat
-        if ((number mod 10) mod 2) = 1 then
-            begin
-                writeln(number mod 10, ' es impar.');
-            end;
-        number := number div 10;
-    until number = 0;
-
+    for i := 0 to 80 do
+        begin
+            write('Introduce un numero: ');
+            readln(number);
+            repeat
+                if ((number mod 10) mod 2) = 1 then
+                    begin
+                        writeln(number mod 10, ' es impar.');
+                    end;
+                number := number div 10;
+            until number = 0;
+        end;
 end.
