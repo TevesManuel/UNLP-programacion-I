9521e488927eaafc8b6eaa1cec505c505ffddfc8
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-03-27 13:39:08 -0300

    2.8.a) solved

---
+++ Practica 2/8/a/app.pas
@@ -0,0 +1,34 @@
+program app;
+
+const
+
+    MAX_LENGHT = 500;
+
+var
+
+    character: char;
+    lenght: integer;
+    wordCounter: integer;
+
+begin
+
+    lenght := 0;
+    wordCounter := 1;//Tiene en cuenta la ultima palabra que termina con '.' y no con ' '
+    
+    Write('Introducir una secuencia de caracteres: ');
+    read(character);
+
+    while (character <> 'z') and (lenght < MAX_LENGHT) do 
+        begin
+            WriteLn('char ', character);
+            read(character);
+            if character = ' ' then
+                begin
+                    wordCounter := wordCounter + 1;
+                end;
+            lenght := lenght + 1;
+        end;
+
+    WriteLn('En total hay ', wordCounter, ' palabras.');
+
+end.
