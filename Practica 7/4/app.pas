97e9df73bc6e51ad0394df784992300de5b09f1d
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-05-01 16:30:36 -0300

    7.4) solved

--- Practica 7/4/app.pas
+++ Practica 7/4/app.pas
@@ -14,33 +14,28 @@ procedure find(v: vector; n: integer; var out: boolean);
 var
     index: integer;
     lastIndex: integer;
+    copyIndex: integer;
 begin
     out := false;
     lastIndex := 0;
     index := v.lenght div 2;
 
-    while ((abs(lastIndex-index) > 1) and (not out)) do
+    while ((abs(lastIndex-index) > 0) and (not out)) do
         begin
-            Writeln(lastIndex);
-            Writeln(index);
-            Writeln(v.arr[index] < n);
-            Writeln(v.arr[index] > n);
-            Writeln(v.arr[index] = n);
             if (v.arr[index] < n) then
                 begin
-                    Writeln('dsa 1');
-                    lastIndex := index;
-                    index := index + ((v.lenght - index) div 2);
+                    copyIndex := index;
+                    index := index + (abs(lastIndex - index) div 2);
+                    lastIndex := copyIndex;
                 end
             else if (v.arr[index] > n) then
                 begin
-                    Writeln('dsa 2');
-                    lastIndex := index;
-                    index := index - (index div 2);
+                    copyIndex := index;
+                    index := index - (abs(lastIndex - index) div 2);
+                    lastIndex := copyIndex;
                 end
             else
                 begin
-                    Writeln('asd');
                     out := true;
                 end;
         end;
@@ -70,5 +65,7 @@ begin
     initializeVector(vec);
     writeVector(vec);
     find(vec, 4, out);
-    // writeln(out);
+    writeln(out);//TRUE
+    find(vec, 12, out);
+    writeln(out);//FALSE
 end.
