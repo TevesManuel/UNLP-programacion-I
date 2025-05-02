a13fd3b8662d76236507d32615023ad8c16a2422
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-05-02 11:03:05 -0300

    7.5) solved

--- Practica 7/5/app.pas
+++ Practica 7/5/app.pas
@@ -1,17 +1,23 @@
 program app;
-// type
-//     matriz: array[1..50, 1..50] of Integer;
 
 var
   matriz: array[1..50, 1..50] of Integer;
   i, j: Integer;
 begin
-  for i := 1 to 50 do
-    for j := 1 to 50 do
-      matriz[i][j] := 0;
-      
-  for i := 1 to 50 do
-    for j := 1 to 50 do
-      Writeln('[', i, '][', j, ']: ', matriz[i][j]);
-      
+    for i := 1 to 50 do
+        for j := 1 to 50 do
+            matriz[i][j] := 0;
+
+    for i := 1 to 50 do
+        begin
+            for j := 1 to 50 do
+                begin
+                    if( (i mod 2) = 0 ) then
+                        Writeln('FILA PAR [', i, '][', j, ']: ', matriz[i][j]);
+                    if( (j mod 2) = 1 ) then
+                        Writeln('COLUMNA IMPAR [', i, '][', j, ']: ', matriz[i][j]);
+                    if( i = j ) then
+                        Writeln('DIAGONAL PRINCIPAL [', i, '][', j, ']: ', matriz[i][j]);
+                end;
+        end;
 end.
